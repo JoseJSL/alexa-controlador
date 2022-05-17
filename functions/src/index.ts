bf7fd0558c78b78b32056790caebbb0ed51916e8
JoseJSL <josehyvy2@gmail.com>
2022-05-17 13:20:00 -0600

    Alexa funcionando con base requests.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,29 @@
 import * as functions from "firebase-functions";
+import { SkillBuilders, } from "ask-sdk-core";
+import { SkillRequestSignatureVerifier, TimestampVerifier } from "ask-sdk-express-adapter";
+import { cancelAndStopIntentHandler, errorHandler, helpIntentHandler, launchRequestHandler, sessionEndedRequestHandler } from './base-request';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const skill = SkillBuilders.custom()
+    .addRequestHandlers(
+        launchRequestHandler,
+        helpIntentHandler,
+        cancelAndStopIntentHandler,
+        sessionEndedRequestHandler,
+    )
+    .addErrorHandlers(errorHandler)
+.create();
+
+exports.alexa = functions.https.onRequest(async (req, res) => {
+    try {
+        const textBody = req.rawBody.toString();
+
+        await new SkillRequestSignatureVerifier().verify(textBody, req.headers);
+        await new TimestampVerifier().verify(textBody);
+        
+        const response = await skill.invoke(req.body);  
+        res.send(response);
+    } catch (err) {
+        console.error(err);
+        res.status(400).send(err);
+    }
+});
